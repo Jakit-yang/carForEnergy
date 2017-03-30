45356d197ea01a529e265ced9a252d8e64b0cd14
qc <qc@qcdeiMac.local>
2017-03-30 19:29:41 +0800

    添加网络请求类

---
+++ MyHTTPMethod.h
@@ -0,0 +1,13 @@
+//
+//  MyHTTPMethod.h
+//  carForEnergy
+//
+//  Created by qc on 2017/3/30.
+//  Copyright © 2017年 新能源. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyHTTPMethod : NSObject
+
+@end
---
+++ MyHTTPMethod.m
@@ -0,0 +1,13 @@
+//
+//  MyHTTPMethod.m
+//  carForEnergy
+//
+//  Created by qc on 2017/3/30.
+//  Copyright © 2017年 新能源. All rights reserved.
+//
+
+#import "MyHTTPMethod.h"
+
+@implementation MyHTTPMethod
+
+@end
