2c625180eea098b110abab6b4acb3d12ecf1db98
qc <qc@qcdeiMac.local>
2017-03-31 10:44:50 +0800

    添加左视图控制器的列表

--- Podfile
+++ Podfile
@@ -9,4 +9,5 @@ pod 'MJRefresh'
 pod 'SDWebImage', '~>3.8'
 pod 'MBProgressHUD', '~> 1.0.0'
 pod 'IQKeyboardManager'
+pod 'Masonry'
 end
--- carForEnergy/LeftViewController.m
+++ carForEnergy/LeftViewController.m
@@ -7,9 +7,12 @@
 //
 
 #import "LeftViewController.h"
-
-@interface LeftViewController ()
-
+#import "Masonry.h"
+#define CELLIDENTIFIER @"listCell"
+@interface LeftViewController ()<UITableViewDelegate,UITableViewDataSource>
+{
+    NSMutableArray *listTitleArray;
+}
 @end
 
 @implementation LeftViewController
@@ -17,6 +20,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self initView];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -25,7 +29,44 @@
 }
 
 - (void)initView {
-    UITableView *tableV = [UITableView alloc]initWithFrame:<#(CGRect)#>;
+    listTitleArray = [NSMutableArray arrayWithObjects:@"租车",@"订单",@"钱包",@"认证",@"消息",@"设置", nil];
+    
+    UITableView *tableV = [[UITableView alloc]init];
+    tableV.delegate = self;
+    tableV.dataSource = self;
+    tableV.backgroundColor = [UIColor whiteColor];
+    [tableV setSeparatorStyle:UITableViewCellSeparatorStyleNone];
+    [self.view addSubview:tableV];
+    
+    [tableV mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.edges.insets(UIEdgeInsetsMake(0, 0, 0, 0));
+    }];
+}
+
+#pragma mark - UITableViewDelegate UITableViewDataSource
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return 60;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return listTitleArray.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CELLIDENTIFIER];
+    if (!cell) {
+        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:CELLIDENTIFIER];
+        cell.selectedBackgroundView = [[UIView alloc] initWithFrame:cell.frame];
+        cell.selectedBackgroundView.backgroundColor = [UIColor orangeColor];
+    }
+    
+    cell.textLabel.text = [listTitleArray objectAtIndex:indexPath.row];
+    
+    return cell;
 }
 
 @end
